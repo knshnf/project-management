3a451e8d162e436dde068b74d1444f6cf899b1cf
Kenshin F <kenshin_fetalvero@dlsu.edu.ph>
2024-06-07 16:50:45 +0800

    Move dashboard filter outside of chart

--- nextjs/components/pages/dashboard/Dashboard.tsx
+++ nextjs/components/pages/dashboard/Dashboard.tsx
@@ -3,6 +3,8 @@ import { pageHeight } from "../../shared_styles/common";
 import Box from "@mui/material/Box";
 import { DatePicker, LocalizationProvider } from "@mui/x-date-pickers";
 import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
+import { useEffect, useState } from "react";
+import dayjs from "dayjs";
 import { Button } from "@mui/material";
 
 import TasksPerStatus from "./charts/tasks/TasksPerStatus";
@@ -12,9 +14,60 @@ import dynamic from "next/dynamic";
 const Chart = dynamic(() => import("react-apexcharts"), { ssr: false });
 
 const Dashboard = () => {
+  const [from_date, setFromDate] = useState(dayjs(new Date(2024, 0)));
+  const [to_date, setToDate] = useState(dayjs(new Date(2024, 11)));
+
   return (
     <Box sx={pageHeight()}>
       <Layout />
+      <Box sx={{
+        display: 'flex',
+        justifyContent: 'space-between',
+        width: "100%",
+        paddingX: 3,
+        marginTop: 3
+      }}>
+        <Box
+          fontWeight={700}
+          textAlign='center'
+        >
+          Task Performance
+        </Box>
+
+        <LocalizationProvider dateAdapter={AdapterDayjs}>
+          <Box
+            sx={{
+              display: "flex",
+              flexDirection: "col",
+              justifyContent: 'flex-end',
+              gap: 1,
+            }}
+          >
+            <DatePicker
+              label={"From"}
+              views={["month", "year"]}
+              sx={{
+                width: "50%",
+              }}
+              slotProps={{ textField: { size: "small" } }}
+              value={dayjs(from_date)}
+              maxDate={dayjs(to_date).set("month", to_date.get("month") - 1)}
+              onChange={(newValue) => setFromDate(newValue)}
+            />
+            <DatePicker
+              label={"To"}
+              views={["month", "year"]}
+              sx={{
+                width: "50%",
+              }}
+              value={dayjs(to_date)}
+              slotProps={{ textField: { size: "small" } }}
+              minDate={dayjs(from_date).set("month", from_date.get("month") + 1)}
+              onChange={(newValue) => setToDate(newValue)}
+            />
+          </Box>
+        </LocalizationProvider>
+      </Box>
       <Box
         sx={{
           display: "flex",
@@ -27,20 +80,21 @@ const Dashboard = () => {
           paddingX: 3
         }}
       >
+        
         <Box
           sx={{
             width: "100%",
             border: '1px solid #C1C7CD',
-            padding: 5
+            padding: 3
           }}
         >
-          {TasksDonePerMonth(new Date(2024, 0), new Date(2024, 11))}
+          <TasksDonePerMonth start_date={from_date} end_date={to_date}/>
         </Box>
         <Box
           sx={{
             width: "100%",
             border: '1px solid #C1C7CD',
-            padding: 5
+            padding: 3
           }}
         >
           {TasksPerStatus()}
--- nextjs/components/pages/dashboard/charts/tasks/TasksDonePerMonth.tsx
+++ nextjs/components/pages/dashboard/charts/tasks/TasksDonePerMonth.tsx
@@ -1,9 +1,9 @@
 import Box from "@mui/material/Box";
 import { DatePicker, LocalizationProvider } from "@mui/x-date-pickers";
 import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
-import { Typography } from "@mui/material";
+import { CircularProgress, Typography } from "@mui/material";
 import { useEffect, useState } from "react";
-import dayjs from "dayjs";
+import dayjs, { Dayjs } from "dayjs";
 
 import { useGetTasksDoneByRangeQuery } from "./queries";
 
@@ -26,31 +26,26 @@ const MONTHS = [
   "Dec",
 ];
 
-const TasksDonePerMonth = (start_date: Date, end_date: Date) => {
-  const [from_date, setFromDate] = useState(dayjs(start_date));
-  const [to_date, setToDate] = useState(dayjs(end_date));
-
+const TasksDonePerMonth = ({start_date, end_date}) => {
   const { loading, error, data, refetch } = useGetTasksDoneByRangeQuery({
     variables: {
-      _lte: to_date.toISOString(),
-      _gte: from_date.toISOString(),
+      _lte: end_date.toISOString(),
+      _gte: start_date.toISOString(),
     },
   });
 
-  const chart_data = {};
-  const curr_date = from_date.toDate();
+  if(loading) {
+    return <CircularProgress/>
+  }
 
-  while (curr_date <= to_date.toDate()) {
-    if (loading) {
-      return;
-    }
-    chart_data[curr_date.toDateString()] = data?.task.filter((task) => {
-      if (
+  const chart_data = {};
+  const curr_date = start_date.toDate();
+
+  while (curr_date <= end_date.toDate()) {
+    chart_data[curr_date.toDateString()] = data?.task.filter((task) => (
         new Date(task.done_date).getMonth() === curr_date.getMonth() &&
         new Date(task.done_date).getFullYear() === curr_date.getFullYear()
-      )
-        return true;
-    }).length;
+    )).length;
 
     curr_date.setMonth(curr_date.getMonth() + 1);
   }
@@ -71,47 +66,12 @@ const TasksDonePerMonth = (start_date: Date, end_date: Date) => {
     {
       name: "Status",
       data: chart_values,
-      // data: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12],
     },
   ];
 
   return (
     <>
-      {/* <Typography> Tasks Per Month </Typography> */}
-      <LocalizationProvider dateAdapter={AdapterDayjs}>
-        <Box
-          sx={{
-            display: "flex",
-            flexDirection: "col",
-            gap: 1,
-            marginBottom: 2,
-          }}
-        >
-          <DatePicker
-            label={"From"}
-            views={["month", "year"]}
-            sx={{
-              width: "30%",
-            }}
-            slotProps={{ textField: { size: "small" } }}
-            value={dayjs(from_date)}
-            maxDate={dayjs(to_date).set("month", to_date.get("month") - 1)}
-            onChange={(newValue) => setFromDate(newValue)}
-          />
-          <DatePicker
-            label={"To"}
-            views={["month", "year"]}
-            sx={{
-              width: "30%",
-            }}
-            value={dayjs(to_date)}
-            slotProps={{ textField: { size: "small" } }}
-            minDate={dayjs(from_date).set("month", from_date.get("month") + 1)}
-            onChange={(newValue) => setToDate(newValue)}
-          />
-        </Box>
-      </LocalizationProvider>
-
+      <Typography> <Box fontWeight={700}> Monthly Finished Tasks </Box> </Typography>
       <Chart options={options} series={series} type="line" width="100%" />
     </>
   );
